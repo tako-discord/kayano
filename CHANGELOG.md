e47ef760c5dc06b896d37adfee6a0641b6ea34d3
Pukimaa <pukima@pukima.site>
2021-10-22 21:53:58 +0200

    [Helper] Initial adding

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+### Added
+- `kayano.js` (helper) (if you run it with node you can start the bot, start the rpc or update much more easily)
+
 ## [1.2.1-alpha] - 2021-10-21
 ### Added
 - Badges in the Readme
---
+++ kayano.js
@@ -0,0 +1,58 @@
+#!/usr/bin/node
+/* eslint-disable prefer-const */
+require('colors');
+const inquirer = require('inquirer');
+
+console.log([
+	'\n',
+	'░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░',
+	'░   ░░░   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░',
+	'▒   ▒▒   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒',
+	'▒   ▒   ▒▒▒▒▒▒▒▒   ▒▒▒▒▒   ▒▒▒   ▒▒▒▒   ▒▒▒▒▒   ▒   ▒▒▒▒▒▒   ▒▒▒▒',
+	'▓  ▓  ▓▓▓▓▓▓▓▓   ▓▓   ▓▓▓   ▓   ▓▓▓   ▓▓   ▓▓▓   ▓▓   ▓▓   ▓▓   ▓',
+	'▓   ▓▓   ▓▓▓▓   ▓▓▓   ▓▓▓▓▓    ▓▓▓   ▓▓▓   ▓▓▓   ▓▓   ▓   ▓▓▓▓   ',
+	'▓   ▓▓▓   ▓▓▓   ▓▓▓   ▓▓▓▓▓▓   ▓▓▓   ▓▓▓   ▓▓▓   ▓▓   ▓▓   ▓▓   ▓',
+	'█   █████   ███   █    ████   ██████   █    █    ██   ████   ████',
+	'██████████████████████████   ████████████████████████████████████',
+	'\n',
+].join('\n'));
+
+inquirer
+	.prompt([
+		{
+			type: 'list',
+			name: 'whatToDo',
+			message: 'What do you want to do? (use your arrow keys)',
+			choices: ['Start the bot', 'Start the RPC ad', 'Update (using ' + 'git'.bgGray + ')', 'Quit'],
+		},
+	])
+	.then(answer => {
+		if (answer.whatToDo == 'Start the bot') {
+			console.info('Attempt to run the file...');
+
+			require('./src/index');
+		}
+
+		if (answer.whatToDo == 'Start the RPC ad') {
+			console.info('Attempt to run the file...');
+
+			require('./rpc_ad/index');
+		}
+
+		if (answer.whatToDo == 'Update (using ' + 'git'.bgGray + ')') {
+			console.info('Attempt to update using git...');
+
+			let exec = require('child_process').exec,
+				update;
+
+			// eslint-disable-next-line no-unused-vars
+			update = exec('git pull && yarn');
+
+			console.info('✔️ Successfully updated and installed dependencies');
+			console.info('Ending job...');
+		}
+
+		if (answer.whatToDo == 'Quit') {
+			return;
+		}
+	});
