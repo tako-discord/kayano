54838ff950bdfe8ed1d9bbe6299542be862f68cc
Pukimaa <pukima@pukima.site>
2021-11-13 15:10:41 +0100

    [registerCommands > testing] Added

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -15,14 +15,26 @@ const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
 (async () => {
 	try {
-		console.log('Started refreshing application (/) commands.');
+		if (process.env.TESTING == 'true') {
+			console.log('Started refreshing application (/) commands. (TESTING)');
 
-		await rest.put(
-			Routes.applicationCommands(process.env.CLIENT_ID),
-			{ body: commands },
-		);
+			await rest.put(
+				Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.TEST_GUILD),
+				{ body: commands },
+			);
 
-		console.log('Successfully reloaded application (/) commands.');
+			console.log('Successfully reloaded application (/) commands. (TESTING)');
+		}
+		else {
+			console.log('Started refreshing application (/) commands.');
+
+			await rest.put(
+				Routes.applicationCommands(process.env.CLIENT_ID),
+				{ body: commands },
+			);
+
+			console.log('Successfully reloaded application (/) commands.');
+		}
 	}
 	catch (error) {
 		console.log('An error occured while refreshing application (/) commands.');
