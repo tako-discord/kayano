677303239bcf3e35f74eba070d3b8912726b0a95
Pukimaa <pukima@pukima.site>
2021-12-07 18:11:32 +0100

    feat(commands): added autopublish
    This also brings in the implementation of the databse (supabase) also you need to specifiy two new
    .env vars with it

---
+++ src/events/messageCreate.js
@@ -0,0 +1,23 @@
+const { supabase } = require('../supabase');
+
+module.exports = {
+	name: 'messageCreate',
+	async execute(message) {
+		if (message.channel.type == 'GUILD_NEWS') {
+			const { data } = await supabase
+				.from('channels')
+				.select('crosspost')
+				.eq('channel_id', `${message.channel.id}`);
+
+			console.log(data);
+
+			if (!data.length) {
+				return;
+			}
+
+			if (data[0].crosspost == true) {
+				message.crosspost();
+			}
+		}
+	},
+};
---
+++ src/supabase.js
@@ -0,0 +1,7 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+module.exports = {
+	supabase,
+};
