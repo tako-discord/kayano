15055b9269097c396477e2215b5c3005d8941ddf
Pukimaa <pukima@pukima.site>
2022-03-31 20:58:15 +0200

    fix(commands > ban): fixed ban command crashing the bot
    This was due to a change in the Discord API which is now implemented

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -7,8 +7,9 @@ module.exports = {
 		.setName('ban')
 		.setDescription('Select a member and ban them')
 		.addUserOption(option => option.setName('target').setDescription('The member to ban').setRequired(true))
-		.addStringOption(option => option.setName('reason').setDescription('The reason you ban the member')),
-	async execute(interaction) {
+		.addStringOption(option => option.setName('reason').setDescription('The reason you ban the member'))
+		.addNumberOption(option => option.setName('days').setDescription('The timespan the messages from the target should be deleted (in days)')),
+	async execute(interaction, client) {
 		if (!interaction.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
 			return await interaction.reply({ content: noPermissionText, ephemeral: true });
 		}
@@ -18,17 +19,20 @@ module.exports = {
 		}
 
 		const target = interaction.options.getMember('target');
+		const user = client.users.fetch(target.id);
 		const reason = interaction.options.getString('reason');
+		const days = (interaction.options.getNumber('days') < 0) ? 0 : (interaction.options.getNumber('days') > 7) ? 7 : Math.round(interaction.options.getNumber('days'));
 		const image = new MessageAttachment('./assets/ban.png', 'ban.png');
 
 		const embed = new MessageEmbed()
 			.setColor(defaultColor)
 			.setThumbnail('attachment://ban.png')
-			.setTitle(`Successfully banned ${target.tag} (${target.id})`)
-			.setDescription(`I successfully banned **${target.username}** for you.`)
-			.addField('Reason:', ((reason) ? reason : 'None'));
+			.setTitle(`Successfully banned ${user.tag || target.user.tag} (${target.id})`)
+			.setDescription(`I successfully banned **${user.username || target.user.username}** for you.`)
+			.addField('Reason:', ((reason) ? reason : 'None'))
+			.addField('The messages that got deleted:', ((days) ? `past ${days} days` : 'None'));
 
-		target.ban(reason);
+		target.ban({ reason: `${reason}`, days: days || 0 });
 		return await interaction.reply({ embeds: [embed], files: [image], ephemeral: true });
 	},
 };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const { Client, Collection, Intents } = require('discord.js');
 const fs = require('fs');
 require('dotenv').config();
 
+console.clear();
 const client = new Client({ intents: [
 	Intents.FLAGS.GUILDS,
 	Intents.FLAGS.GUILD_MESSAGES,
 	Intents.FLAGS.GUILD_MEMBERS,
+	Intents.FLAGS.GUILD_BANS,
 ], allowedMentions: {
 	parse: ['users', 'roles'],
 	repliedUser: false,
